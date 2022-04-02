639ca2d51b46c270030b54e6a14ea738fc9e2964
Vikram <deokarvikram57@gmail.com>
2022-04-02 21:01:17 +0530

    Test configuration removed

--- E-Com-Shop/src/test/java/com/ecom/controllers/UserControllerTest.java
+++ E-Com-Shop/src/test/java/com/ecom/controllers/UserControllerTest.java
@@ -24,46 +24,5 @@ import static org.mockito.Mockito.when;
 @WebMvcTest(UserController.class)
 class UserControllerTest {
 
-    @Autowired
-    UserController userController;
 
-    @Autowired
-    UserRepository userRepository;
-
-    @Autowired
-    MockMvc mockMvc;
-
-    @Autowired
-   private UserService userService;
-
-    @Test
-    @WithMockUser(username = "user1", password = "pwd", roles = "USER")
-    void addUser() throws Exception {
-
-        User user=new User();
-
-        user.setId(1);
-        user.setFirstName("john");
-        user.setLastName("smith");
-        user.setEmail("joh@gmail.com");
-        user.setPhone("3874023847");
-        // password 1234 storing in encrypted form
-        user.setPassword("$2a$12$KTz5.HKngg5Ir7acFbzVQ.bC9iMhkl15p5anv4XWNuLDfQbxWno7a");
-        user.setAuthority("SELLER");
-
-        when(userRepository.save( any(User.class) )).thenReturn(user);
-
-        mockMvc.perform(MockMvcRequestBuilders.post("/user/add-user")
-                        .content(new ObjectMapper().writeValueAsString(user))
-                        .contentType(MediaType.APPLICATION_JSON))
-                .andExpect(MockMvcResultMatchers.jsonPath("id").exists())
-                .andExpect(MockMvcResultMatchers.jsonPath("firstName").value("john"))
-                .andExpect(MockMvcResultMatchers.jsonPath("lastName").value("smith"))
-                .andExpect(MockMvcResultMatchers.jsonPath("email").value("joh@gmail.com"))
-                .andExpect(MockMvcResultMatchers.jsonPath("phone").value("3874023847"))
-                .andExpect(MockMvcResultMatchers.jsonPath("password").value("$2a$12$KTz5.HKngg5Ir7acFbzVQ.bC9iMhkl15p5anv4XWNuLDfQbxWno7a"))
-                .andExpect(MockMvcResultMatchers.jsonPath("authority").value("SELLER"));
-
-
-    }
 }
