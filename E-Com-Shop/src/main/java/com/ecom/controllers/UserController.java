b2fdf59975c6477a0602733b5c45134eaf61789e
Vikram <deokarvikram57@gmail.com>
2022-04-03 11:58:17 +0530

    addUser() return type changed and test passed

--- E-Com-Shop/src/main/java/com/ecom/controllers/UserController.java
+++ E-Com-Shop/src/main/java/com/ecom/controllers/UserController.java
@@ -9,6 +9,8 @@ import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 
@@ -31,7 +33,7 @@ public class UserController {
             "otherwise it will return 401 unauthorized access." +
             "Username would be email and password is what user was set while creating user. If user is created then it will return true otherwise false")
     @PostMapping("/add-user")
-    public boolean addUser(@RequestBody User newuser)
+    public ResponseEntity<User> addUser(@RequestBody User newuser)
     {
         String pass=newuser.getPassword();
 
@@ -42,10 +44,9 @@ public class UserController {
         User user=userRepository.save(newuser);
 
 
-
         if(user==null)
-            return false;
-        return true;
+            new ResponseEntity<>(user, HttpStatus.BAD_REQUEST);
+        return new ResponseEntity<>(user, HttpStatus.CREATED);
     }
 
 
--- E-Com-Shop/src/test/java/com/ecom/controllers/UserControllerTest.java
+++ E-Com-Shop/src/test/java/com/ecom/controllers/UserControllerTest.java
@@ -4,6 +4,7 @@ import com.ecom.config.UserService;
 import com.ecom.models.Products;
 import com.ecom.models.User;
 import com.ecom.repository.UserRepository;
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -21,6 +22,7 @@ import java.util.List;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(UserController.class)
 class UserControllerTest {
@@ -38,16 +40,28 @@ class UserControllerTest {
     UserService userService;
 
     @Test
-    public  void addUser()
-    {
+    public  void addUser() throws Exception {
         User user=new User();
 
         user.setId(1);
         user.setName("john");
         user.setEmail("j@gmail.com");
         user.setPhone("84736495837");
-        user.setAuthority("SELLER");
+       // user.setAuthority("SELLER");
         user.setPassword("1234");
+
+        when(userRepository.save(any(User.class))).thenReturn(user);
+
+        mockMvc.perform(MockMvcRequestBuilders.post("/user/add-user")
+                .content(new ObjectMapper().writeValueAsString(user))
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isCreated())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.id").exists())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.name").value("john"))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.email").value("j@gmail.com"))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.phone").value("84736495837"))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.password").value("1234"));
+
 
 
     }
